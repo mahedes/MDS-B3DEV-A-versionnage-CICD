c17e52f43422ddaf7ed116f2b4115491c5d59ec2
Kagialpha <steefkagi@gmail.com>
2025-11-03 15:58:17 +0100

    css modif

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 const input = document.querySelector(".new-todo");
-const list = document.querySelector(".todo-list");
+const list = document.querySelector(".todo-list"); // <ul> cible
 const counter = document.querySelector(".todo-count");
 
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     const task = input.value.trim();
     if (task === "") return;
+
     const li = document.createElement("li");
-    li.textContent = task;
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = task;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    deleteBtn.classList.add("destroy");
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+      updateCount();
+    });
+
+    li.appendChild(textSpan);
+    li.appendChild(deleteBtn);
+
     list.appendChild(li);
 
     input.value = "";
